7b7cfbcfad9408c4c13387fbfc595a8617be0884
naye-0 <nasidkayevhenii@gmail.com>
2025-06-16 20:47:28 +0200

    Task: Install ArrayAccess for models

--- htdocs/src/Post/PostModel.php
+++ htdocs/src/Post/PostModel.php
@@ -1,12 +1,34 @@
-<?php 
+<?php
 
 namespace App\Post;
 
-class PostModel
+use ArrayAccess;
+
+class PostModel implements ArrayAccess
 {
+
     public $id;
     public $title;
     public $content;
-}
 
-?>
+    public function offsetExists(mixed $offset): bool
+    {
+      return isset($this->$offset);
+    }
+
+    public function offsetGet(mixed $offset): mixed
+    {
+      return $this->$offset;
+    }
+
+    public function offsetSet(mixed $offset, mixed $value): void
+    {
+      $this->$offset = $value;
+    }
+
+    public function offsetUnset(mixed $offset): void
+    {
+      unset($this->$offset);
+    }
+
+}
