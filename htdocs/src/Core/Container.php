d36c930c357a8f314ff1f91f51aa79c4302a364c
naye-0 <nasidkayevhenii@gmail.com>
2025-07-03 20:21:44 +0200

    Container 2

--- htdocs/src/Core/Container.php
+++ htdocs/src/Core/Container.php
@@ -10,23 +10,30 @@ class Container
 
     public function getPdo()
     {
+        if (!empty($this->pdo)) {
+            return $this->pdo;
+        }
         // Create a new PDO connection to the MySQL database
-        $pdo = new PDO(
+        $this->pdo = new PDO(
             'mysql:host=php-blog.dvl.to;dbname=php-blog;charset=utf8', // DSN (Data Source Name)
             'php-blog',                // Database username
             'CA97/IdIZRUvH(EM'         // Database password
             );
 
             // Disable emulation of prepared statements for better security
-            $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-            return $pdo;
+            $this->pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+            return $this->pdo;
     }
 
     public function getPostsRepository()
     {
-        return new PostsRepository(
+        if (!empty($this->postsRepository)) {
+            return $this->postsRepository;
+        }
+        $this->postsRepository = new PostsRepository(
             $this->getPdo()
         );
+        return $this->postsRepository;
     }
 }
 
